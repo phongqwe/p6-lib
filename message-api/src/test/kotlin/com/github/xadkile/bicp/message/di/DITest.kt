5d97c6eed82e03124e9525e6f735117ae6c8e5d7
Phong <xadkile@gmail.com>
2021-11-14 21:27:48 +0700

    misc

--- message-api/src/test/kotlin/com/github/xadkile/bicp/message/di/DITest.kt
+++ message-api/src/test/kotlin/com/github/xadkile/bicp/message/di/DITest.kt
@@ -1,21 +1,23 @@
 package com.github.xadkile.bicp.message.di
 
+import com.github.michaelbull.result.Ok
 import com.github.xadkile.bicp.message.api.connection.IPythonContextImp
 import com.github.xadkile.bicp.test.utils.TestResource
 import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 class DITest {
     @Test
     fun test(){
-//        val component = DaggerMessageApiComponent.builder()
-//            .ipythonContext(TestResource.ipythonConfigForTest()).build()
+        val component = DaggerMessageApiComponent
+            .builder()
+            .ipythonConfig(TestResource.ipythonConfigForTest())
+            .build()
 
-//        val context = component.ipythonContext()
+        val context = component.ipythonContext()
 
-//        context.startIPython()
-//        assertTrue(context.getChannelProvider() is Ok)
-//
-//        val abc = component.abc()
-//        context.stopIPython()
+        context.startIPython()
+        assertTrue(context.getChannelProvider() is Ok)
+        context.stopIPython()
     }
 }
