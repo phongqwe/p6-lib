8526d9f52512b50b2a1dfa7313128e49579e1234
Phong <xadkile@gmail.com>
2021-11-28 14:28:30 +0700

    fix blocking bug in CodeExecutionSender.kt + add test for edge cases

--- message-api/src/main/kotlin/com/github/xadkile/bicp/message/api/msg/sender/composite/CodeExecutionSender.kt
+++ message-api/src/main/kotlin/com/github/xadkile/bicp/message/api/msg/sender/composite/CodeExecutionSender.kt
@@ -3,6 +3,7 @@ package com.github.xadkile.bicp.message.api.msg.sender.composite
 import com.github.michaelbull.result.*
 import com.github.xadkile.bicp.message.api.connection.kernel_context.KernelContextReadOnly
 import com.github.xadkile.bicp.message.api.connection.kernel_context.KernelContextReadOnlyConv
+import com.github.xadkile.bicp.message.api.connection.kernel_context.KernelIsDownException
 import com.github.xadkile.bicp.message.api.msg.listener.MsgHandlers
 import com.github.xadkile.bicp.message.api.msg.listener.MsgListener
 import com.github.xadkile.bicp.message.api.msg.protocol.message.JPMessage
@@ -20,61 +21,67 @@ typealias ExecuteResult = JPMessage<IOPub.ExecuteResult.MetaData, IOPub.ExecuteR
 
 class CodeExecutionSender(
     val kernelContext: KernelContextReadOnlyConv,
-    val executeSender:MsgSender<ExecuteRequest, Result<ExecuteReply, Exception>>,
-    val ioPubListener: MsgListener
+    val executeSender: MsgSender<ExecuteRequest, Result<ExecuteReply, Exception>>,
+    val ioPubListener: MsgListener,
 ) : MsgSender<ExecuteRequest, Result<ExecuteResult, Exception>> {
 
     override suspend fun send(
         message: ExecuteRequest,
         dispatcher: CoroutineDispatcher,
     ): Result<ExecuteResult, Exception> {
-        return this.checkContextRunningThen {
-            var rt: Result<ExecuteResult, Exception>? = null
-            // p: config listener
-            ioPubListener.also { listener ->
-                listener.addHandler(
-                    MsgHandlers.withUUID(IOPub.ExecuteResult.msgType) {m,l->
-                        val receivedMsg: ExecuteResult = m.toModel()
-                        if (receivedMsg.parentHeader == message.header) {
-                            rt = Ok(receivedMsg)
-                            listener.stop()
-                        }
-                    }
-                )
-            }
-            ioPubListener.use {
-                coroutineScope {
-                    // rmd: start the iopub listener, it is on a separated coroutine.
-                    val startRs=ioPubListener.start(this, dispatcher)
-                    val listenerStartOk = startRs is Ok
-                    if(listenerStartOk){
-                        launch(dispatcher) {
-                            // rmd: wait until ioPubListener to go online
-                            Sleeper.waitUntil { ioPubListener.isRunning() }
-                            val sendStatus = executeSender.send(message, Dispatchers.Default)
-                            val sendOk = sendStatus is Ok
-                            val sendRes:Result<MsgStatus,Exception> = if(sendOk){
-                                val msgOk: Boolean = sendStatus.get()!!.content.status == MsgStatus.ok
-                                if (msgOk) {
-                                    Ok(MsgStatus.ok)
-                                } else {
-                                     Err(UnableToSendMsgException(message))
-                                }
-                            }else{
-                                Err(sendStatus.unwrapError())
-                            }
-                            if (sendRes is Err) {
-                                rt = sendRes
-                            }
-                        }
-                    }else{
-                        rt = Err(startRs.unwrapError())
+
+        if(kernelContext.isNotRunning()){
+            return Err(KernelIsDownException.occurAt(this))
+        }
+
+        var rt: Result<ExecuteResult, Exception>? = null
+        // p: config listener
+        ioPubListener.also { listener ->
+            listener.addHandler(
+                MsgHandlers.withUUID(IOPub.ExecuteResult.msgType) { m, l ->
+                    val receivedMsg: ExecuteResult = m.toModel()
+                    if (receivedMsg.parentHeader == message.header) {
+                        rt = Ok(receivedMsg)
+                        listener.stop()
                     }
                 }
-                Sleeper.waitUntil { rt != null }
-                rt!!
+            )
+        }
+        coroutineScope {
+            // rmd: start the iopub listener, it is on a separated coroutine.
+            val startRs =  ioPubListener.start(this, dispatcher)
+            if (startRs is Ok) {
+                Sleeper.waitUntil { ioPubListener.isRunning() }
+                launch(dispatcher) {
+                    // rmd: wait until ioPubListener to go online
+                    val sendStatus = executeSender.send(message, dispatcher)
+                    val sendRes: Result<MsgStatus, Exception> = if (sendStatus is Ok) {
+                        val msgOk: Boolean = sendStatus.get()!!.content.status == MsgStatus.ok
+                        if (msgOk) {
+                            Ok(MsgStatus.ok)
+                        } else {
+                            Err(UnableToSendMsgException(message))
+                        }
+                    } else {
+                        Err(sendStatus.unwrapError())
+                    }
+                    if (sendRes is Err) {
+                        rt = sendRes
+                        ioPubListener.stop()
+                    }
+                }
+            }
+            else {
+                rt = Err(startRs.unwrapError())
+                ioPubListener.stop()
             }
         }
+        Sleeper.waitUntil {
+            rt != null
+        }
+        ioPubListener.stop()
+       return rt!!
+
     }
 
     override fun getKernelContext(): KernelContextReadOnly {
--- message-api/src/test/kotlin/com/github/xadkile/bicp/message/api/msg/sender/composite/CodeExecutionSenderTest.kt
+++ message-api/src/test/kotlin/com/github/xadkile/bicp/message/api/msg/sender/composite/CodeExecutionSenderTest.kt
@@ -1,21 +1,52 @@
 package com.github.xadkile.bicp.message.api.msg.sender.composite
 
-import com.github.michaelbull.result.Ok
-import com.github.michaelbull.result.unwrap
+import com.github.michaelbull.result.*
+import com.github.xadkile.bicp.message.api.connection.kernel_context.KernelContextReadOnly
+import com.github.xadkile.bicp.message.api.connection.kernel_context.KernelIsDownException
 import com.github.xadkile.bicp.message.api.msg.listener.IOPubListener
+import com.github.xadkile.bicp.message.api.msg.listener.MsgListener
 import com.github.xadkile.bicp.message.api.msg.protocol.message.data_interface_definition.Shell
+import com.github.xadkile.bicp.message.api.msg.sender.MsgSender
+import com.github.xadkile.bicp.message.api.msg.sender.exception.UnableToSendMsgException
+import com.github.xadkile.bicp.message.api.msg.sender.shell.ExecuteReply
 import com.github.xadkile.bicp.message.api.msg.sender.shell.ExecuteRequest
 import com.github.xadkile.bicp.message.api.msg.sender.shell.ExecuteSender
 import com.github.xadkile.bicp.test.utils.TestOnJupyter
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.TestInstance
+import java.lang.IllegalStateException
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class CodeExecutionSenderTest : TestOnJupyter() {
+
+
+    @AfterEach
+    fun ae(){
+        kernelContext.stopKernel()
+    }
+    val message: ExecuteRequest = ExecuteRequest.autoCreate(
+        sessionId = "session_id",
+        username = "user_name",
+        msgType = Shell.Execute.msgType,
+        msgContent = Shell.Execute.Request.Content(
+            code = "x=1+1*2;y=x*2;y",
+            silent = false,
+            storeHistory = true,
+            userExpressions = mapOf(),
+            allowStdin = false,
+            stopOnError = true
+        ),
+        "msg_id_abc_123"
+    )
 
     /**
      * TODO write more edge case test:
@@ -27,29 +58,16 @@ internal class CodeExecutionSenderTest : TestOnJupyter() {
         runBlocking {
             kernelContext.startKernel()
             kernelContext.getHeartBeatService().unwrap().start()
-            val message: ExecuteRequest = ExecuteRequest.autoCreate(
-                sessionId = "session_id",
-                username = "user_name",
-                msgType = Shell.Execute.msgType,
-                msgContent = Shell.Execute.Request.Content(
-                    code = "x=1+1*2;y=x*2;y",
-                    silent = false,
-                    storeHistory = true,
-                    userExpressions = mapOf(),
-                    allowStdin = false,
-                    stopOnError = true
-                ),
-                "msg_id_abc_123"
-            )
+
             val sender = CodeExecutionSender(
                 kernelContext = kernelContext.conv(),
                 executeSender = ExecuteSender(kernelContext.conv()),
                 ioPubListener = IOPubListener(
                     kernelContext = kernelContext.conv(),
-                    defaultHandler = { msg,l ->
+                    defaultHandler = { msg, l ->
                         println(msg)
                     },
-                    parseExceptionHandler = { e,l ->
+                    parseExceptionHandler = { e, l ->
                         println(e)
                     }
                 )
@@ -68,23 +86,48 @@ internal class CodeExecutionSenderTest : TestOnJupyter() {
             kernelContext.startKernel()
             kernelContext.getHeartBeatService().unwrap().start()
 
-            val message: ExecuteRequest = ExecuteRequest.autoCreate(
-                sessionId = "session_id",
-                username = "user_name",
-                msgType = Shell.Execute.msgType,
-                msgContent = Shell.Execute.Request.Content(
-                    code = "x=1+1*2;y=x*2;y",
-                    silent = false,
-                    storeHistory = true,
-                    userExpressions = mapOf(),
-                    allowStdin = false,
-                    stopOnError = true
-                ),
-                "msg_id_abc_123"
-            )
-//            val sender = CodeExecutionSender(kernelContext.conv())
-//            val o = sender.send(message, Dispatchers.Default)
-//            assertTrue(o is Ok, o.toString())
+            // p: mockk is horribly slow here
+            val mockSender = object : MsgSender<ExecuteRequest, Result<ExecuteReply, Exception>> {
+                override fun getKernelContext(): KernelContextReadOnly {
+                    TODO("Not yet implemented")
+                }
+
+                override suspend fun send(
+                    message: ExecuteRequest,
+                    dispatcher: CoroutineDispatcher,
+                ): Result<ExecuteReply, Exception> {
+                    return Err(UnableToSendMsgException(message))
+                }
+            }
+
+            val sender = CodeExecutionSender(kernelContext.conv(), mockSender, IOPubListener(kernelContext))
+            val o = sender.send(message, Dispatchers.Default)
+            assertTrue(o is Err, o.toString())
+            assertTrue(o.unwrapError() is UnableToSendMsgException)
+            assertEquals(message, (o.unwrapError() as UnableToSendMsgException).msg)
         }
+    }
+
+    @Test
+    fun send_kernelNotRunning()= runBlocking {
+        kernelContext.stopKernel()
+        val sender = CodeExecutionSender(kernelContext.conv(),ExecuteSender(kernelContext.conv()),IOPubListener(kernelContext))
+        val o = sender.send(message)
+        assertTrue(o is Err)
+        assertTrue((o.unwrapError()) is KernelIsDownException)
+    }
+
+    @Test
+    fun send_unableToStartListener() = runBlocking{
+        kernelContext.startKernel()
+        val mockListener = mockk<MsgListener>().also {
+            coEvery{it.start(any(),any())} returns Err(IllegalStateException())
+            every { it.addHandler(any()) } returns Unit
+            coEvery { it.stop() } returns Unit
+        }
+        val sender = CodeExecutionSender(kernelContext.conv(),ExecuteSender(kernelContext.conv()),mockListener)
+        val o = sender.send(message)
+        assertTrue(o is Err)
+        assertTrue((o.unwrapError()) is IllegalStateException)
     }
 }
