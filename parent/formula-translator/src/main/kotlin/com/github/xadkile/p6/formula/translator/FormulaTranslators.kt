2618c9fb737a97378fb7c7a6a5c12605abdff7df
Phong <xadkile@gmail.com>
2022-03-09 20:55:03 +0700

    misc: - misc updates

--- parent/formula-translator/src/main/kotlin/com/github/xadkile/p6/formula/translator/FormulaTranslators.kt
+++ parent/formula-translator/src/main/kotlin/com/github/xadkile/p6/formula/translator/FormulaTranslators.kt
@@ -2,4 +2,5 @@ package com.github.xadkile.p6.formula.translator
 
 object FormulaTranslators {
     val standard: FormulaTranslator = FormulaTranslatorFinal()
+    val directLiteralTranslator:FormulaTranslator = DirectLiteralTranslator()
 }
